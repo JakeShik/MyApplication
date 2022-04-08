722915ffdc94b335f68dbf323982a3539d8794d4
1700948 <jaha5825@gmail.com>
2022-04-08 11:26:54 +0100

    Show error message when the messageBox isn't fullfilled
    Signed-off-by: 1700948 <jaha5825@gmail.com>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonEnviar = findViewById<Button>(R.id.buttonEnviar)
+        buttonEnviar.setOnClickListener { enviaMensagem() }
+
+
+    }
+
+    private fun enviaMensagem() {
+
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+        val mensagem = editTextMensagem.text.toString()
+
+        if(mensagem.isBlank()){
+            editTextMensagem.error = "Deve preencher a mensagem"
+            editTextMensagem.requestFocus()
+            return
+        }
+
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+
+        intent.putExtra("MENSAGEM", mensagem)
+
+        startActivity(intent)
     }
 }
+
+
+
